2e8b6fcba502b95dc55dba827ed747d9da115a0b
Oskar R <o.rajzner@gmail.com>
2022-10-14 23:37:05 +0200

    First commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public double addAtoB(double a, double b){
+        return a + b;
+    }
+
+    public double subtractionAtoB(double a, double b){
+        return a - b;
+    }
+}
---
+++ src/main/java/com/kodilla/Main.java
@@ -0,0 +1,9 @@
+package com.kodilla;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.addAtoB(5.3,5.8));
+        System.out.println(calc.subtractionAtoB(5.3,5.8));
+    }
+}
